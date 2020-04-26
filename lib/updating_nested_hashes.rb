fe8d33376beb49060236244e07aebc013e6e5b0f
breezy-desk-5482 <rmcclorey1@gmail.com>
2020-04-26 18:05:15 +0000

    Automatically backed up by Learn

--- lib/updating_nested_hashes.rb
+++ lib/updating_nested_hashes.rb
@@ -60,9 +60,8 @@ def add_characters
    #   - The second hash should have the :name key set to "Apothecary" 
    # Code your solution here:
 
-
-
-
+epic_tragedy = {additional_characters:[
+  name: "Prince Escalus", "Apothecary"]}
 
 
    # After your code, the following line will return the altered hash
